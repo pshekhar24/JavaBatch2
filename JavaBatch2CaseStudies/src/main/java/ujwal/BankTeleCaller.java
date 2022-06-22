a9293f5599ee669c1f903aa78acba457179dc26c
ujwal-venkatesh <107928420+ujwal-venkatesh@users.noreply.github.com>
2022-06-22 11:16:37 +0530

    adding assignments files

---
+++ JavaBatch2CaseStudies/src/main/java/ujwal/BankTeleCaller.java
@@ -0,0 +1,189 @@
+package training.com;
+import java.util.Scanner;
+public class BankTeleCaller 
+{
+    public static void main(String[] args) {
+        mainMenu();
+    }
+    private static void mainMenu() {
+        System.out.println("press 0 to exit\n" + "press 1 for Loans\n" +"press 2 for Saving\n" + "press 3 for Current\n"+"press 4 for cards\n");
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+
+        switch (input){
+            case 0:
+                System.out.println("Stop Processing");
+                break;
+            case 1:
+                loans();
+                break;
+            case 2:
+                savingAccount();
+                break;
+            case 3:
+                currentAccount();
+                break;
+            case 4:
+            	cards();
+            	break;
+        }
+        sc.close();
+    }                                                   
+    private static void savingAccount() {
+        System.out.println("please enter your savings account number");
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        if(a.matches("\\d{8}")){
+            System.out.println("Thank you for account number\n" +"press 1 to show account balance\n"+ "press 2 to speak customer care\n" +"press 9 to go back to previous menu");
+            int x = sc.nextInt();
+            switch (x){
+                case 1:
+                    System.out.println("show account balance");
+                    break;
+                case 2:
+                    System.out.println("customer care Number 123445676");
+                    break;
+                case 3:
+                    mainMenu();
+                    break;
+                case 9:
+                	mainMenu();
+                	break;
+            }
+            sc.close();
+        }
+        else {
+            System.out.println("Invalid account number, Press 0 to try again or Press 9 to go back to previous menu");
+            Scanner sc1 = new Scanner(System.in);
+            int b = sc1.nextInt();
+            if( b == 0) {
+                savingAccount();
+            }
+            else if(b==9) {
+            	mainMenu();
+            }
+            sc1.close();
+        }
+    }
+    private static void loans() {
+        System.out.println("press 1 for personal loan\n" +"press 2 for housing loan\n" +"press 3 for car loan\n" +"press 4 for gold loan\n" +"press 9 to go back to previous menu");
+        Scanner sc = new Scanner(System.in);
+        int savingsInput =  sc.nextInt();
+
+        switch (savingsInput){
+            case 1:
+                System.out.println("personal loan");
+                break;
+            case 2:
+                System.out.println("housing loan");
+                break;
+            case 3:
+                System.out.println("car loan");
+                break;
+            case 4:
+                System.out.println("gold loan");
+                break;
+            case 9:
+                mainMenu();
+                break;
+        }
+        sc.close();
+    }
+    private static void currentAccount() {
+    	System.out.println("please enter your current account number");
+    	Scanner sc=new Scanner(System.in);
+    	String  a=sc.nextLine();
+    	
+    	if(a.matches("\\d{8}")) {
+    		System.out.println("Thank you for account number\n" +"press 1 to show account balance\n"+ "press 2 to speak customer care\n" +"press 9 to go back to previous menu");
+    		int y=sc.nextInt();
+    		switch(y){
+    		case 1:
+    			System.out.println("show account balance");
+    			break;
+    		case 2:
+    			System.out.println("customer care number 123456789");
+    		    break;
+    		case 3:
+    			mainMenu();
+    			break;
+    		 case 9:
+                 mainMenu();
+                 break;	
+    		}
+    		sc.close();
+    	}
+    	else {
+            System.out.println("Invalid account number, Press 0 to try again or Press 9 to go back to previous number");
+            Scanner sc1 = new Scanner(System.in);
+            int b = sc1.nextInt();
+            if( b == 0) {
+                currentAccount();
+            }
+            else if(b==9) {
+            	mainMenu();
+            }
+            sc1.close();
+        }
+    }
+    private static void cards() {
+    	System.out.println("press 1 for credit card\n"+"press 2 for debit card\n");
+    	Scanner sc= new Scanner(System.in);
+        int input = sc.nextInt();
+    	
+    			switch(input) {
+    			case 1:
+    				creditCard();
+    				break;
+    			case 2:
+    				debitCard();
+    			    break;	
+    			}
+    		sc.close();
+    	}
+	
+	private static void creditCard() {
+		System.out.println("please enter your debit card number");
+    	Scanner sc =new Scanner(System.in);
+    	String  a=sc.nextLine();
+		
+    	if(a.matches("\\d{16}")) {
+    		System.out.println("Thank you for account number\n" +"press 1 to block your card\n"+ "press 2 to speak customer care\n" +"press 9 to go back to previous menu");
+    		int y=sc.nextInt();
+    		switch(y){
+    		case 1:
+    			System.out.println("card is blocked");
+    			break;
+    		case 2:
+    			System.out.println("customer care number 123456789");
+    		    break;
+    		 case 9:
+                 mainMenu();
+                 break;	
+    		}
+    		sc.close();
+    	}
+    }
+	private static void debitCard() {
+		System.out.println("please enter your credit card number");
+    	Scanner sc =new Scanner(System.in);
+    	String  a=sc.nextLine();
+		
+    	if(a.matches("\\d{16}")) {
+    		System.out.println("Thank you for account number\n" +"press 1 to block your card\n"+ "press 2 to speak customer care\n" +"press 9 to go back to previous menu");
+    		int y=sc.nextInt();
+    		switch(y){
+    		case 1:
+    			System.out.println("card is blokced");
+    			break;
+    		case 2:
+    			System.out.println("customer care number 123456789");
+    		    break;
+    		 case 9:
+                 mainMenu();
+                 break;	
+    		}
+    		sc.close();
+    	}
+	}
+}
